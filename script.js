7d2a259862add34b2d2b642420c48d2b16737e0e
lolekmakso <makstarasovv1@gmail.com>
2023-02-18 23:30:13 +0200

    power for phone

---
+++ script.js
@@ -0,0 +1,6 @@
+function navToggle() {
+    var headerLinks = document.getElementById("headerLinks");
+    var menuIcon = document.getElementById("menuIcon");
+    menuIcon.classList.toggle("_active");
+    headerLinks.classList.toggle("_active");
+  }
